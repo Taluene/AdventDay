b510fa9ebcdfbf607b5fd526f9813231d3d99e72
Kevin Pratt <prattkev@gmail.com>
2021-12-21 23:16:43 -0500

    Day 1 part 1 and 2

--- AdventDay1/Program.cs
+++ AdventDay1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventDay1
 {
@@ -6,35 +8,31 @@ namespace AdventDay1
     {
         static void Main(string[] args)
         {
+            //var fil = File.ReadAllLines(Path.GetFullPath("text.txt"));
+            //int.TryParse(fil[0], out int depth);
+
+            int[] depth = File.ReadAllLines(Path.GetFullPath("text.txt")).Select(int.Parse).ToArray();
+            int[] depthbatch = File.ReadAllLines(Path.GetFullPath("text2.txt")).Select(int.Parse).ToArray();
+
+
             int increased = 0;
-            int[] depth = new int[10] { 199,
-                                            200,
-                                            208,
-                                            210,
-                                            200,
-                                            207,
-                                            240,
-                                            269,
-                                            260,
-                                            263
-                                             };
 
 
             for (int i = 0; i < depth.Length; i++)
                 if (i == 0)
                 {
-                         Console.Write(depth[i]);
-                         Console.WriteLine(" Nothing to do Fam");
+                    Console.Write(depth[i]);
+                    Console.WriteLine(" Nothing to do Fam");
                 }
-                
-                
-                
+
+
+
                 else if (depth[i] > depth[i - 1])
-                   { 
-                         Console.Write(depth[i]);
-                         Console.WriteLine(" Increased");
-                          increased += 1;
-                    }
+                {
+                    Console.Write(depth[i]);
+                    Console.WriteLine(" Increased");
+                    increased += 1;
+                }
                 else
                 {
 
@@ -42,9 +40,55 @@ namespace AdventDay1
                     Console.WriteLine(" Decreased");
                 }
 
+            //Part 2 Loop
+
+            Console.WriteLine(" BATCH TIME BITCH");
+
+            var BatchIncreased = 0;
+
+            for (int i = 0; i < depth.Length - 2; i++)
+                if (i == 0)
+                {
+                    Console.Write(depth[i]);
+                    Console.WriteLine(" Nothing to do Fam");
+                }
+
+
+
+                else if ((depth[i] + depth[i+1] + depth[i + 2]) > (depth[i-1] + depth[i] + depth[i + 1]))
+                {
+                    Console.Write(depth[i]);
+                    Console.WriteLine(" Increased");
+                    BatchIncreased += 1;
+                }
+
+                else if ((depth[i] + depth[i + 1] + depth[i + 2]) == (depth[i - 1] + depth[i] + depth[i + 1]))
+                {
+                    Console.Write(depth[i]);
+                    Console.WriteLine(" No change");
+                   
+                }
+
+                else
+                {
+
+                    Console.Write(depth[i]);
+                    Console.WriteLine(" Decreased");
+                }
+
+
+
+
+
+
+
             Console.WriteLine();
             Console.Write("Total number of increases in survey area = ");
             Console.WriteLine(increased);
+
+            Console.WriteLine();
+            Console.Write("Total number of increases in batch survey area = ");
+            Console.WriteLine(BatchIncreased);
 
 
         }
